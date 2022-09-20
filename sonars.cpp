59f518a15542b77125405379400738f184eeca08
Michael Zaramensky <zaram@yandex.ru>
2022-09-20 10:24:23 +0300

    headers

--- sonars.cpp
+++ sonars.cpp
@@ -13,6 +13,8 @@
 #include <fstream>
 #include <vector>
 #include <string>
+
+
 #include <unistd.h>			//Used for UART
 #include <fcntl.h>			//Used for UART
 #include <termios.h>	    // for UART
@@ -24,6 +26,9 @@
 
 #include "debout.h"
 #include "Task.h"
+
+#include "uart.hpp"
+#include "sonar.hpp"
 
 
 struct _modbus_get_data {
@@ -40,289 +45,6 @@ struct _modbus_get_data {
     void *backend_data;
 };
 
-
-class SonarDev{
-public:
-    SonarDev(){}
-    SonarDev(int id_, int test_reg_, int measure_reg_):id(id_), test_reg(test_reg_), measure_reg(measure_reg_), error_counter(0){
-        // valid = is_alive();
-    }
-
-    bool is_alive(){
-        bool res = false;
-        int rc;
-        uint16_t tab_reg[2];
-        modbus_flush(ctx);
-        modbus_set_slave(ctx, id);
-        rc = modbus_read_registers(ctx, test_reg, 1, tab_reg);
-        res = (rc != -1);
-        return res;
-    }
-
-    bool measure(){
-        bool res = false;
-        distance = -1;
-        int rc;
-        uint16_t tab_reg[2];
-        modbus_set_slave(ctx, id);
-        rc = modbus_read_registers(ctx, measure_reg, 1, tab_reg);
-
-        if(rc == 1){
-            res = true;
-            error_counter = 0;
-            distance = tab_reg[0];
-        }else{
-             ++error_counter;
-        }
-        return res;
-    }
-
-    int get_id(){
-        return id;
-    }
-
-    int get_error_counter(){
-        return error_counter;
-    }
-
-    uint16_t get_distance(){
-        return distance;
-    }
-
-    void put_ctx(modbus_t *ctx_){
-        ctx = ctx_;
-    }
-
-private:
-    modbus_t *ctx;
-    int id;
-    int test_reg;
-    int measure_reg;
-    int error_counter;
-    int16_t distance;
-    bool valid;
-};
-
-
-class ModbusCtx{
-public:
-
-    ModbusCtx(){};
-    ModbusCtx(std::string s_port_, int baud_, char parity_, int data_bit_, int stop_bit_, uint32_t time_out_, uint32_t boot_time_):
-        s_port(s_port_), parity(parity_), baud(baud_), data_bit(data_bit_), stop_bit(stop_bit_), time_out(time_out_){
-
-        port = s_port.c_str();
-        boot_time = std::chrono::milliseconds(boot_time_);
-        valid = (Modbus_connect() == 1);
-    }
-
-    // ~ModbusCtx(){
-    //     modbus_free(ctx);
-    // }
-
-    int Modbus_connect(){
-        ctx = modbus_new_rtu(port, baud, parity, data_bit, stop_bit);
-        if (ctx == NULL) {
-            debug_counter("sonar error:", "Unable to create the libmodbus context");
-            valid = false;
-            return -1;
-        }
-
-        if (modbus_connect(ctx) == -1) {
-            fprintf(stderr, "sonar error: Connection failed: %s \n",  modbus_strerror(errno));
-            modbus_free(ctx);
-            valid = false;
-            return -1;
-        }
-
-        std::this_thread::sleep_for(boot_time);
-        modbus_set_response_timeout(ctx, 0, time_out);
-        modbus_rtu_set_serial_mode(ctx, MODBUS_RTU_RS485);
-        return 1;
-    }
-
-    bool get_valid(){
-        return valid;
-    }
-
-    void add_device(SonarDev dev){
-        dev.put_ctx(ctx);
-        devices.push_back(dev);
-    }
-
-    std::vector<SonarDev> get_devices(){
-        return devices;
-    }
-
-    std::string get_port(){
-
-        return s_port;
-    }
-
-private:
-    modbus_t *ctx;
-    std::vector<SonarDev> devices;
-    std::chrono::milliseconds boot_time = 0ms;
-    bool valid;
-    const char *port;
-    std::string s_port;
-    char parity;
-    int baud;
-    int data_bit;
-    int stop_bit;
-    uint32_t time_out;
-};
-
-class UartCtx{
-public:
-    UartCtx(){}
-    UartCtx(std::string port_, char parity_, int baud_, int data_bit_, int stop_bit_):
-        s_port(port_), parity(parity_), baud(baud_), data_bit(data_bit_), stop_bit(stop_bit_){
-
-            uart_connect();
-            err_counter=0;
-        }
-        ~UartCtx(){ close(serial_port);}
-
-        void uart_connect(){
-            serial_port = open(s_port.c_str(), O_RDWR);
-            valid = (tcgetattr(serial_port, &tty) != 0);
-            switch (parity)
-            {
-            case 'N':
-            case 'n':
-                tty.c_cflag &= ~PARENB; // Clear parity bit, disabling parity (most common)
-                break;
-            case 'O':
-            case 'o':
-                tty.c_cflag |= PARENB;
-                tty.c_cflag |= PARODD; // odd
-                break;
-            case 'E':
-            case 'e':
-                tty.c_cflag |= PARENB;
-                tty.c_cflag &= ~PARODD; // even
-                break;
-            default:
-                tty.c_cflag &= ~PARENB;
-                break;
-            }
-
-            switch (stop_bit)
-            {
-            case 1:
-                tty.c_cflag &= ~CSTOPB; // Clear stop field, only one stop bit used in communication (most common)
-                break;
-            default:
-                tty.c_cflag |= CSTOPB;
-                break;
-            }
-
-            tty.c_cflag &= ~CSIZE; // Clear all bits that set the data size
-
-            switch (data_bit)
-            {
-            case 8:
-                tty.c_cflag |= CS8; // 8 bits per byte (most common)
-                break;
-            case 7:
-                tty.c_cflag |= CS7; // 7 bits per byte
-                break;
-            case 6:
-                tty.c_cflag |= CS6; // 6 bits per byte
-                break;
-            case 5:
-                tty.c_cflag |= CS5; // 5 bits per byte
-                break;
-
-            default:
-                tty.c_cflag |= CS8; // 8 bits per byte (most common)
-                break;
-            }
-
-            tty.c_cflag &= ~CRTSCTS; // Disable RTS/CTS hardware flow control (most common)
-            tty.c_cflag |= CREAD | CLOCAL; // Turn on READ & ignore ctrl lines (CLOCAL = 1)
-
-            tty.c_lflag &= ~ICANON;
-            tty.c_lflag &= ~ECHO; // Disable echo
-            tty.c_lflag &= ~ECHOE; // Disable erasure
-            tty.c_lflag &= ~ECHONL; // Disable new-line echo
-            tty.c_lflag &= ~ISIG; // Disable interpretation of INTR, QUIT and SUSP
-            tty.c_iflag &= ~(IXON | IXOFF | IXANY); // Turn off s/w flow ctrl
-            tty.c_iflag &= ~(IGNBRK|BRKINT|PARMRK|ISTRIP|INLCR|IGNCR|ICRNL); // Disable any special handling of received bytes
-
-            tty.c_oflag &= ~OPOST; // Prevent special interpretation of output bytes (e.g. newline chars)
-            tty.c_oflag &= ~ONLCR; // Prevent conversion of newline to carriage return/line feed
-            // tty.c_oflag &= ~OXTABS; // Prevent conversion of tabs to spaces (NOT PRESENT ON LINUX)
-            // tty.c_oflag &= ~ONOEOT; // Prevent removal of C-d chars (0x004) in output (NOT PRESENT ON LINUX)
-
-            tty.c_cc[VTIME] = 10;    // Wait for up to 1s (10 deciseconds), returning as soon as any data is received.
-            tty.c_cc[VMIN] = 0;
-
-            // Set in/out baud rate to be 9600
-            // cfsetispeed(&tty, B9600);
-            // cfsetospeed(&tty, B9600);
-            tty.c_ispeed = baud;
-            tty.c_ospeed = baud;
-
-            // Save tty settings, also checking for error
-            if (tcsetattr(serial_port, TCSANOW, &tty) != 0) {
-                valid = false;
-                debprint("Error %i from tcsetattr: %s\n", errno, strerror(errno));
-            }
-        }
-
-        bool measure(){
-            bool res = false;
-            memset(&read_buf, '\0', sizeof(read_buf));
-            num_bytes = read(serial_port, &read_buf, sizeof(read_buf));
-            for(int i = 3; i < num_bytes; ++i){
-                uint8_t check_sum = read_buf[i];
-                uint8_t head = read_buf[i-3];
-                uint8_t byte_h = read_buf[i-2];
-                uint8_t byte_l = read_buf[i-1];
-
-                if(check_sum != ((head+byte_h+byte_l)&0xFF)){
-                    ++err_counter;
-                    continue;
-                }
-                res = true;
-                dist = (byte_h << 8) | byte_l;
-            }
-            return res;
-        }
-
-        bool get_valid(){
-            return valid;
-        }
-
-        uint get_distance(){
-            return dist;
-        }
-
-        std::string get_port(){
-            return s_port;
-        }
-
-        int get_error_counter(){
-            return err_counter;
-        }
-
-private:
-    bool valid;
-    int serial_port;
-    std::string s_port;
-    char parity;
-    uint8_t read_buf [256];
-    int baud;
-    int data_bit;
-    int stop_bit;
-    struct termios tty;
-    int num_bytes;
-    uint dist;
-    int err_counter;
-
-};
 
 
 
@@ -565,7 +287,7 @@ int main(int argc, char *argv[])
     w_begin = std::chrono::steady_clock::now();
     double w_time = 0.;
 
-    while (w_time <= 20.) {
+    while (w_time <= 200.) {
         w_time = std::chrono::duration_cast<std::chrono::seconds>(std::chrono::steady_clock::now() - w_begin).count();
 
         for(auto line:mb_lines ){
